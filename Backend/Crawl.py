4619f51506bd2fbc9489803115fedeceaf3bf5e6
RsiWater <ijn951963@gmail.com>
2020-06-11 14:19:31 +0800

    test

---
+++ Backend/Crawl.py
@@ -0,0 +1,81 @@
+import requests
+from selenium import webdriver
+from bs4 import BeautifulSoup 
+from WeatherClass import *
+
+url = 'https://www.cwb.gov.tw/V8/C/W/County/index.html'
+driver = webdriver.PhantomJS(executable_path = 'C:/Users/ijn95/OneDrive/桌面/Code/python/phantomjs-2.1.1-windows/bin/phantomjs.exe')
+driver.get(url)
+soup = BeautifulSoup(driver.page_source, "lxml")
+
+pageBlock = soup.find('div', class_ = 'weather_data')
+cityIndex = list()
+
+weatherData = dict()
+
+for cityBlock in pageBlock:
+    print(cityBlock.attrs['name'])
+    cityIndex.append(cityBlock.attrs['name'])
+
+for index in cityIndex:
+    element = soup.find(attrs={"name" : index})
+    targetAddress = url.split('/V8', 1)[0] +  element.get('href')
+    print(targetAddress)
+
+    driver.get(targetAddress)
+    subSoup = BeautifulSoup(driver.page_source, 'lxml')
+    
+    city = subSoup.find('h2', class_ = 'main-title').text
+    information_day = subSoup.find_all('span', class_ = 'Day')
+    information_night = subSoup.find_all('span', class_ = "Night")
+    dates = subSoup.find_all('span', class_ = 'date')
+
+    weather, temperature = [], []
+
+    weatherList = list()
+
+    for i in range(len(information_day)):
+
+        data_day = information_day[i].find_all()
+        data_night = information_night[i].find_all()
+        data_date = dates[i].find_all()
+
+        for element in data_day:
+            print(element)
+        for element in data_night:
+            print(element)
+        for element in data_date:
+            print(element)
+
+        weather.append(data_day[1].get('alt'))
+        temperature.append(data_day[2].string)
+        
+        for j in range(2):
+            temp_d = Weather()
+            temp_d.city = city
+            if j == 0:
+                data = data_day
+                temp_d.period = "Day"
+            else:
+                data = data_night
+                temp_d.period = "Night"
+            temp_d.situation = data[1].get('alt')
+            temp_d.max_temperature = int(data[2].string[:2])
+            temp_d.min_temperature = int(data[2].string[5:])
+            temp_d.month = int(data_date[1].string[:2])
+            temp_d.day = int(data_date[1].string[3:])
+            weatherList.append(temp_d)
+
+        
+    
+        # print(dates[i])
+        print('\n\n')
+
+    print(weather)
+    print(temperature)
+    print(weatherList)
+
+    weatherData[city] = weatherList
+print('456')
+# for elements in soup.find_all('table', class_ = "table"):
+#     print(elements.get('tr')) 
---
+++ Backend/db_class_account.py
@@ -0,0 +1,137 @@
+import random
+import sqlite3
+
+
+
+class Account:
+    item,detail,receipt,note,dbfile,query='','','','','',''
+    year,month,day,money,number,status,key,con,selectnum,operationCode=0,0,0,0,0,0,0,0,0,0
+
+    def set_item(self, item):  #set分類
+        self.item=item
+    def get_item(self):
+        return self.item
+    def set_year(self, year):  #set年
+        self.year=year
+    def get_year(self):
+        return self.year
+    def set_month(self, month):  #set月
+        self.month=month
+    def get_month(self):
+        return self.month
+    def set_day(self, day):  #set日
+        self.day=day
+    def get_day(self):
+        return self.day
+    def set_money(self, money):  #set金額
+        self.money=money
+    def get_money(self):
+        return self.money
+    def randomize_number(self):        #set id(insert時使用)
+        self.number=random.randint(0,99999)
+    def set_number(self, number):
+        self.number = number
+    def get_number(self):
+        return self.number
+    def set_detail(self, detail):  #set細項
+        self.detail=detail
+    def get_detail(self):
+        return self.detail
+    def set_receipt(self, receipt):  #set發票
+        self.receipt=receipt
+    def get_receipt(self):
+        return self.receipt
+    def set_note(self, note):        #set備註
+        self.note=note
+    def get_note(self):
+        return self.note
+    def set_status(self, status):    #set收支屬性
+        self.status=status
+    def get_status(self):
+        return self.status
+    def set_key(self,key):            #set id(刪除、修改、查詢時使用)
+        self.key=key
+    def get_key(self):
+        return self.key
+    def set_operationCode(self, code):
+        self.operationCode = code
+    def get_operaionCode(self):
+        return self.operationCode
+    # def set_query(self,query):        #set要求(修改時使用)
+    #     self.query=query
+    # def get_query(self):
+    #     return self.query
+    # def set_selectnum(self,selectnum):  #set選取到的id(查詢時使用)
+    #     self.selectnum=selectnum
+    # def get_selectnum(self):
+    #     return self.selectnum
+
+    def operateAction(self):
+        if self.operationCode == 0: # 新增
+            self.insert()
+            print("insert success.")
+        elif self.operationCode == 1: # 刪除
+            self.key = self.number
+            self.delete()
+            print("delete success.")
+        elif self.operationCode == 2: # 修改
+            self.key = self.number
+            self.delete()
+            self.insert()
+            print("modify success.")
+        elif self.operationCode == 3: # 查詢
+            self.select()
+            print("select success.")
+    def DEBUG_printAllAttribute(self):
+        print("ID", self.number)
+        print("money", self.money)
+        print("year", self.year)
+        print("month", self.month)
+        print("day", self.day)
+        print('item', self.item)
+        print('detail', self.detail)
+        print('receipt', self.receipt)
+        print('status', self.status)
+        print('note', self.note)
+
+
+    def __init__(self):
+        self.dbfile='life.db'
+        self.con=sqlite3.connect(self.dbfile)
+    def insert(self):
+        self.DEBUG_printAllAttribute()
+        self.con.execute('insert into record(金額,年,月,日,分類,細項,發票,備註,收支屬性,id)values({},{},{},{},"{}","{}","{}","{}",{},{});'.format(self.money,self.year,self.month,self.day,self.item,self.detail,self.receipt,self.note,self.status,self.number))
+        self.con.commit()
+    def delete(self):
+        self.con.execute('delete from record where id={};'.format(self.key))
+        self.con.commit()
+    def update(self):
+        self.con.execute('delete from record where id={};'.format(self.key))
+        self.con.execute('insert into record(金額,年,月,日,分類,細項,發票,備註,收支屬性,id)values({},{},{},{},"{}","{}","{}","{}",{},{});'.format(self.money,self.year,self.month,self.day,self.item,self.detail,self.receipt,self.note,self.status,self.key))
+        self.con.commit()
+    def select(self):
+        datas=[]
+        data=self.con.execute('select * from record where id={};'.format(self.key))
+        for i in data:
+            for j in i:
+                datas.append(j)
+        self.set_money(datas[0])
+        self.set_year(datas[1])
+        self.set_month(datas[2])
+        self.set_day(datas[3])
+        self.set_item(datas[4])
+        self.set_detail(datas[5])
+        self.set_receipt(datas[7])
+        self.set_note(datas[8])
+        self.set_status(datas[9])
+        print("select success")
+    def close(self):
+        self.con.close()
+    
+
+        
+    
+    
+    
+        
+        
